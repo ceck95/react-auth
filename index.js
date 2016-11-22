85e0489d4e33156ef7582062950d3bfa9e876b9f
nhutdev <tranvannhut4495@gmail.com>
2016-11-22 14:38:15 +0700

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * @Author: Tran Van Nhut <nhutdev>
+ * @Date:   2016-11-22T13:51:14+07:00
+ * @Email:  tranvannhut4495@gmail.com
+ * @Last modified by:   nhutdev
+ * @Last modified time: 2016-11-22T13:51:14+07:00
+ */
+
+
+
+'use strict';
+
+module.exports = {
+  AuthWrapper: require('./lib/auth')
+};
