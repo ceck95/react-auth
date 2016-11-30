162b3df4998fac63fd49a9ac65870c20ec29aff0
nhutdev <tranvannhut4495@gmail.com>
2016-11-30 17:00:25 +0700

    update libs

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @Author: Tran Van Nhut <nhutdev>
  * @Date:   2016-11-22T13:51:14+07:00
  * @Email:  tranvannhut4495@gmail.com
- * @Last modified by:   nhutdev
- * @Last modified time: 2016-11-22T13:51:14+07:00
+* @Last modified by:   nhutdev
+* @Last modified time: 2016-11-25T10:26:19+07:00
  */
 
 
