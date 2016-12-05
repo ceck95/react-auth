f69ffcdcf5cb39c16b8e81479c1c7b2e6b116492
nhutdev <tranvannhut4495@gmail.com>
2016-12-05 11:39:41 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @Date:   2016-11-22T13:51:14+07:00
  * @Email:  tranvannhut4495@gmail.com
 * @Last modified by:   nhutdev
-* @Last modified time: 2016-12-01T10:24:35+07:00
+* @Last modified time: 2016-12-05T11:39:26+07:00
  */
 
 
@@ -11,6 +11,5 @@
 'use strict';
 
 module.exports = {
-  AuthWrapper: require('./lib/auth'),
-  ContainerBase: require('./lib/container')
+  AuthWrapper: require('./lib/auth')
 };
