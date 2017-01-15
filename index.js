1a82423515f4c26b1fa6743a6012485f4df38977
nhutdev <tranvannhut4495@gmail.com>
2017-01-15 18:35:36 +0700

    add container

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @Author: Tran Van Nhut <nhutdev>
  * @Date:   2016-11-22T13:51:14+07:00
  * @Email:  tranvannhut4495@gmail.com
-* @Last modified by:   nhutdev
-* @Last modified time: 2016-12-05T11:39:26+07:00
+ * @Last modified by:   nhutdev
+ * @Last modified time: 2016-12-05T12:42:30+07:00
  */
 
 
@@ -11,5 +11,6 @@
 'use strict';
 
 module.exports = {
-  AuthWrapper: require('./lib/auth')
+  AuthWrapper: require('./lib/auth'),
+  ContainerBase: require('./lib/container')
 };
