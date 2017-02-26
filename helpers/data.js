aab704db2afef24c69265dbabe72b2020f371e8b
nhutdev <tranvannhut4495@gmail.com>
2017-02-26 12:59:03 +0700

    add assign data

--- helpers/data.js
+++ helpers/data.js
@@ -3,7 +3,7 @@
  * @Date:   2017-02-26T12:36:04+07:00
  * @Email:  tranvannhut4495@gmail.com
 * @Last modified by:   nhutdev
-* @Last modified time: 2017-02-26T12:52:29+07:00
+* @Last modified time: 2017-02-26T12:58:55+07:00
  */
 
 'use strict';
@@ -15,13 +15,6 @@ class Data {
     opts = opts || {};
 
     src = src || {};
-    opts = Hoek.applyToDefaults({
-      override: false,
-      keepDest: false,
-      excepts: [],
-      attributes: [],
-      prefix: null
-    }, opts);
 
     var srcKey = '';
 
@@ -43,7 +36,7 @@ class Data {
         }
 
       } else {
-        var underKey = stringer.toUnderscore(key);
+        let underKey = Data.toUnderscore(key);
         srcKey = opts.prefix ? (opts.prefix + '_' + underKey) : underKey;
 
         if (src.hasOwnProperty(srcKey) && opts.excepts.indexOf(key) === -1 && (opts.attributes.length > 0 ? opts.attributes.indexOf(key) > -1 : true)) {
@@ -62,6 +55,10 @@ class Data {
       }
 
     });
+  }
+
+  static toUnderscore(input) {
+    return input.replace(/([A-Z])/g, "_$1").replace(/^_/, '').toLowerCase();
   }
 
 }
