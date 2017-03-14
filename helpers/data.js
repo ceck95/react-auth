b9c701fa612ebdbcede95244db5905de528d69ad
nhutdev <tranvannhut4495@gmail.com>
2017-03-14 14:42:54 +0700

    add func data

--- helpers/data.js
+++ helpers/data.js
@@ -2,8 +2,8 @@
  * @Author: Tran Van Nhut <nhutdev>
  * @Date:   2017-02-26T12:36:04+07:00
  * @Email:  tranvannhut4495@gmail.com
-* @Last modified by:   nhutdev
-* @Last modified time: 2017-02-26T14:18:12+07:00
+* @Last modified by:   root
+* @Last modified time: 2017-03-14T14:40:19+07:00
  */
 
 'use strict';
@@ -18,6 +18,23 @@ class Data {
 
   static toUnderscore(input) {
     return input.replace(/([A-Z])/g, "_$1").replace(/^_/, '').toLowerCase();
+  }
+
+  static isEmail(email) {
+    let regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regexp.test(email);
+  }
+
+  static isPhoneNumber(phone) {
+    let regexp = /^[\s()+-]*([0-9][\s()+-]*){9,10}$/;
+    return regexp.test(phone);
+  }
+
+  static isEmpty(string) {
+    if (string.trim() === '') {
+      return true;
+    }
+    return false;
   }
 
 }
